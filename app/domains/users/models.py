763442a2fd70950d5954d754b7210a760282b4f5
jcaviedesc <jcamilocc97@gmail.com>
2022-08-27 11:02:30 -0500

    feat(users): add endpoint to list patients by professional, refactor and improve code

--- app/domains/users/models.py
+++ app/domains/users/models.py
@@ -1,38 +1,66 @@
 from datetime import datetime, date
 from typing import Optional
 from dateutil.relativedelta import relativedelta
-from pydantic import BaseModel, Field, PositiveFloat, HttpUrl
+from pydantic import BaseModel, Field, PositiveFloat, HttpUrl, PositiveInt
 from ...core.baseModel import DatatimeModelMixin, IDModelMixin
-from .schemas import  UnitModel, UserBaseSchema, HealthQuestionsModel
+from .schemas import  UnitModel
 from .enums import GenderEnum, HealthInfoEnum, AsystemUnitEnum, CardiovascularRiskOption, UserTypeEnum
 
+class UserMeasurementModel(BaseModel):
+    name: str
+    value: float = Field(..., alias="v")
+    unit: str = Field(..., alias="u")
+    last_measurement: datetime | str = Field(..., alias="lst_msrmnt")
+    status: str
 
-class UserCreateBase(IDModelMixin, DatatimeModelMixin, UserBaseSchema):
-    age: Optional[int]
-    imc: Optional[PositiveFloat]
+    class Config:
+        allow_population_by_field_name = True
+
+class UserCreateBase(IDModelMixin, DatatimeModelMixin):
+    name: str = Field(...)
+    last_name: str = Field(...)
+    doc_id: str = Field(...)
+    phone: str = Field(...)
+    address: Optional[str]
+    birthdate: date = Field(...)
+    location: Optional[list[int]] = Field(None, max_items=2, min_items=2)
+    sex: GenderEnum = Field(...)
+    role: UserTypeEnum = Field(...)
+    profession: Optional[str]
+    occupation: Optional[str] = Field(None, min_length=3)
+    avatar: Optional[HttpUrl] = Field(None)
+    age: Optional[PositiveInt]
+    bmi: Optional[PositiveFloat]
+    measurements: Optional[list[UserMeasurementModel]]
     
     def calculate_age(self):
         time_difference = relativedelta(datetime.utcnow(), self.birthdate)
         self.age = time_difference.years
         return self
 
-    def calculate_IMC(self):
+    def calculate_body_mass_index(self, height: UnitModel, weight: UnitModel):
         # TODO use unit for calculate IMC in accordance to system unit
         # Fórmula: peso (kg) / [estatura (m)]2
-        height = self.height.val
-        if self.height.unit == AsystemUnitEnum.centimeter:
-            height = self.height.val / 100
-        self.imc = round(self.weight.val / height**2, 2)
+        bmi_height = height.val
+        if height.unit == AsystemUnitEnum.centimeter:
+            bmi_height = height.val / 100
+        self.bmi = round(weight.val / bmi_height**2, 2)
         return self
 
+    def set_initial_measurements(self, height: UnitModel, weight: UnitModel):
+        timestamp = datetime.utcnow()
+        height_measurement = UserMeasurementModel(name="height", v=height.val, u=height.unit, lst_msrmnt=timestamp, status="normal")
+        weight_measurement = UserMeasurementModel(name="weight", v=weight.val, u=weight.unit, lst_msrmnt=timestamp, status="normal")
+        blood_pressure = UserMeasurementModel(name="blood pressure", v=0, u="mmHg", lst_msrmnt="", status="no data")
+        
+        self.measurements = [height_measurement, weight_measurement, blood_pressure]
 
 class PatientUserCreate(UserCreateBase):
-    health_questions: HealthQuestionsModel = Field(..., alias="questions")
     role: int = Field(UserTypeEnum.patient, const=True)
-    cardiovascular_risk: Optional[CardiovascularRiskOption] = Field(None, alias='cardiovascular_risk')
+    cardiovascular_risk: Optional[CardiovascularRiskOption]
 
-    def set_cardiovascular_risk(self):
-        if HealthInfoEnum.yes in self.health_questions.dict().values():
+    def set_cardiovascular_risk(self, health_questions: dict):
+        if HealthInfoEnum.yes in health_questions.values():
             self.cardiovascular_risk = CardiovascularRiskOption.HIGHT
         else:
             self.cardiovascular_risk = CardiovascularRiskOption.LOW
--- app/domains/users/repository.py
+++ app/domains/users/repository.py
@@ -11,7 +11,10 @@ class UserRepository(BaseRepository):
     All database actions associated with the User resource
     """
     async def create_user(self, *, user: PatientUserCreate | ProfessionalUserCreate, exclude_fields: Optional[list[str]] = None) -> UserCreatedModel | None:
-        user_body = jsonable_encoder(user, by_alias=True, exclude_unset=True, exclude_none=True)
+        """returns a UserCreatedModel if insert into database is succesfull otherwise None.
+        """
+        user_body = jsonable_encoder(
+            user, by_alias=True, exclude_none=True)
         try:
             new_user = await self.get_entity('Users').insert_one(user_body)
         except:
@@ -25,6 +28,21 @@ class UserRepository(BaseRepository):
         projection = make_excluded_fields(exclude_fields)
         user_result = await self.get_entity('Users').find_one({"_id": ObjectId(id)}, projection)
         return UserCreatedModel(**user_result) if not user_result is None else None
+
+    async def get_patients(self, *, professional_id: str, page_size: int, page_num: int):
+        """returns a set of Users documents belonging to page number `page_num`
+        where size of each page is `page_size`.
+        """
+        # Calculate number of documents to skip
+        skips = page_size * (page_num - 1)
+        query = {
+            "linked_professionals": professional_id,
+        }
+        projection = make_excluded_fields()
+        cursor = await self.get_entity('Users').find(query, projection).skip(skips).limit(page_size)
+
+        return [UserCreatedModel(**patient) for patient in cursor]
+
     # async def update_user(self, *, user: UserCreate | UserUpdate, user_id: str, exclude_fields: Optional[list[str]] = None) -> UserPublic | None:
     #     user_body = jsonable_encoder(user, by_alias=True)
     #     updated_user = await self.get_entity('Users').update_one({'_id': user_id}, {'$set': user_body, '$currentDate': {'upAt': True}})
